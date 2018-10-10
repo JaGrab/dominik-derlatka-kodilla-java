25f71b04e419804ab83ecf6a94b803f63f699be0
JaGrab <dominik.derlatka@gmail.com>
2018-10-10 16:37:41 +0200

    New module calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,30 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania.");
+        System.out.println("Testing SimpleUser constructor");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if(result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Testing Calculator class:");
+        Calculator calculator = new Calculator();
+        if(calculator.add(-3, 2) == -1){
+            System.out.println("Method add OK");
+        } else {
+            System.out.println("Method add error");
+        }
+        if(calculator.subtract(-5, -7) == 2) {
+            System.out.println("Method subtract OK");
+        } else {
+            System.out.println("Method subtract error");
+        }
     }
 }
