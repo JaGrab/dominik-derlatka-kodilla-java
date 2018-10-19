0259523219a2efb0d1f297e7f54f8897a2905acc
JaGrab <dominik.derlatka@gmail.com>
2018-10-19 16:25:22 +0200

    One stream to rule them all

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -5,11 +5,12 @@ import java.util.stream.IntStream;
 public interface ArrayOperations {
     static double getAverage(int[] numbers){
         System.out.println("Array elements: ");
-        IntStream.range(0, numbers.length)
-                .map(n -> numbers[n])
-                .forEach(System.out::println);
+//        IntStream.range(0, numbers.length)
+//                .map(n -> numbers[n])
+//                .forEach(System.out::println);
         double average = IntStream.range(0, numbers.length)
                 .map(n -> numbers[n])
+                .peek(System.out::println)
                 .average()
                 .getAsDouble();
         return average;
