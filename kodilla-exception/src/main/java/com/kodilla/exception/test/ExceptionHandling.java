7ef6c6f84ac6abb7df7d927bf9e9914811f86872
JaGrab <dominik.derlatka@gmail.com>
2018-10-23 20:54:35 +0200

    Exception throwing

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void  main (String args[]) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            System.out.println(secondChallenge.probablyIWillThrowException(2.0, 3.0));
+        } catch (Exception e) {
+            System.out.println("I caught an exception... probably: " + e);
+        } finally {
+            System.out.println("The exception hunting is over");
+        }
+    }
+}
