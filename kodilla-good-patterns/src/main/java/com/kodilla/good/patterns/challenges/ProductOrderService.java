8f2ee6837453e84bc2d468a00407febef1a97299
JaGrab <dominik.derlatka@gmail.com>
2018-10-31 23:31:12 +0100

    Class names correction

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
@@ -1,27 +1,11 @@
 package com.kodilla.good.patterns.challenges;
 
 public class ProductOrderService {
-    private InformationService informationService;
-    private OrderService orderService;
-    private OrderRepository orderRepository;
-
-    public ProductOrderService(final InformationService informationService,
-                           final OrderService orderService,
-                           final OrderRepository orderRepository) {
-        this.informationService = informationService;
-        this.orderService = orderService;
-        this.orderRepository = orderRepository;
-    }
-    public OrderDto process(final OrderRequest orderRequest) {
-        boolean isOrdered = orderService.order(orderRequest.getUser(), orderRequest.getArticle(),
-                orderRequest.getQuantity());
-
-        if(isOrdered) {
-            informationService.inform(orderRequest.getUser());
-            orderRepository.createOrder(orderRequest.getUser(), orderRequest.getArticle(), orderRequest.getQuantity());
-            return new OrderDto(orderRequest.getUser(), true);
-        } else {
-            return new OrderDto(orderRequest.getUser(), false);
-        }
+    public static void main(String[] args) {
+        OrderRequestRetriever orderRequestRetriever = new OrderRequestRetriever();
+        OrderRequest orderRequest = orderRequestRetriever.retrieve();
+        OrderProcessor productOrderService = new OrderProcessor(
+                new InformationServiceEmail(), new StandardOrderService(), new SqliteOrderRepository());
+        productOrderService.process(orderRequest);
     }
 }
