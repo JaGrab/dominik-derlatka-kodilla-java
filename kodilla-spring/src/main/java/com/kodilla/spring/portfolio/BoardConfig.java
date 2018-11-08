2792ef5e8f2eb799f07d9507fe40cccaee1ddb18
JaGrab <dominik.derlatka@gmail.com>
2018-11-08 22:02:31 +0100

    Qualifier version

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,31 +1,35 @@
 package com.kodilla.spring.portfolio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BoardConfig {
     @Autowired
+    @Qualifier("list1")
     private TaskList toDoList;
     @Autowired
+    @Qualifier("list2")
     private TaskList inProgressList;
     @Autowired
+    @Qualifier("list3")
     private TaskList doneList;
 
     @Bean
     public Board getBoard() {
         return new Board(toDoList, inProgressList, doneList);
     }
-    @Bean(name = "toDoList")
+    @Bean(name = "list1")
     public TaskList getToDoList() {
         return new TaskList();
     }
-    @Bean(name = "inProgressList")
+    @Bean(name = "list2")
     public TaskList getInProgressList() {
         return new TaskList();
     }
-    @Bean(name = "doneList")
+    @Bean(name = "list3")
     public TaskList getDoneList() {
         return new TaskList();
     }
