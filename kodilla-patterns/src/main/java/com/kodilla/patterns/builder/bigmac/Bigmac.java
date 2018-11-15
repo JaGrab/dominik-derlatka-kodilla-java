9acffbdc0cbe96e644488ac2395e07d3e7b01ce6
JaGrab <dominik.derlatka@gmail.com>
2018-11-15 21:06:33 +0100

    Private constructor

--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/Bigmac.java
@@ -84,7 +84,7 @@ public class Bigmac {
             }
         }
     }
-    public Bigmac(final String bun, final int burgers, final String sauce, final List<String> ingredients) {
+    private Bigmac(final String bun, final int burgers, final String sauce, final List<String> ingredients) {
         this.bun = bun;
         this.burgers = burgers;
         this.sauce = sauce;
