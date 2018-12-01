f645013f159b1bf409cba35fce25c288c339dfb0
JaGrab <dominik.derlatka@gmail.com>
2018-12-01 19:42:00 +0100

    HQL and native SQL named queries

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -9,11 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CompanyDaoTestSuite {
     @Autowired
     CompanyDao companyDao;
+    @Autowired
+    EmployeeDao employeeDao;
 
     @Test
     public void testSaveManyToMany(){
@@ -56,6 +60,42 @@ public class CompanyDaoTestSuite {
             companyDao.delete(softwareMachineId);
             companyDao.delete(dataMaestersId);
             companyDao.delete(greyMatterId);
+        } catch (Exception e) {
+            //do nothing
+        }
+    }
+    @Test
+    public void testNameQueries() {
+        //Given
+        Employee employee1 = new Employee("John", "Doe");
+        Employee employee2 = new Employee("Jane", "Doe");
+        Employee employee3 = new Employee("Connor", "MacLeod");
+        Company company1 = new Company("Datalogic");
+        Company company2 = new Company("Dataminer");
+        Company company3 = new Company("Highlander Ltd.");
+        company1.getEmployees().add(employee1);
+        company2.getEmployees().add(employee2);
+        company3.getEmployees().add(employee3);
+        employee1.getCompanies().add(company1);
+        employee2.getCompanies().add(company2);
+        employee3.getCompanies().add(company3);
+        companyDao.save(company1);
+        companyDao.save(company2);
+        companyDao.save(company3);
+
+        //When
+        List<Employee> employeesDoe = employeeDao.retrieveEmployeeWithLastname("Doe");
+        List<Company> companiesDat = companyDao.retrieveCompanyBeginningWith("Dat");
+
+        //Then
+        Assert.assertEquals(2, employeesDoe.size());
+        Assert.assertEquals(2, companiesDat.size());
+
+        //CleanUp
+        try {
+            companyDao.delete(company1.getId());
+            companyDao.delete(company2.getId());
+            companyDao.delete(company3.getId());
         } catch (Exception e) {
             //do nothing
         }
